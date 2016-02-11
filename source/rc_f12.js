50fd55d2b0a188ee22a59f3d70509a654e2e7bc8
ProjectHTML <salehsonn@gmail.com>
2016-02-11 17:46:10 +0100

    Create rc_f12.js

---
+++ source/rc_f12.js
@@ -0,0 +1,13 @@
+if (document.addEventListener)
+{
+    document.addEventListener('contextmenu', function(e)
+    {
+           e.preventDefault();
+    }, false);
+}
+else
+{
+    document.attachEvent('oncontextmenu', function()
+    {
+            window.event.returnValue = false;
+    });
