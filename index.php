030665e243b6a1060dad32749742dd20d47641ae
ProjectHTML <salehsonn@gmail.com>
2016-02-12 20:56:29 +0100

    Changes were made at index.php
    Now can you even drag the box, The PHP is beeing developed, But notice that it wont be great PHP coded, This is just a BETA.

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
+<?php
+/*session_start();
+if(isset($_SESSION['user']))
+{
+	header("location: play.php");
+}
+else
+{
+	mysql_connect("localhost", "root", "");
+	mysql_select_db("ph");
+	
+} */
+?>
 <!doctype HTML>
 <html>
 	<head>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		 <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css" />
+        <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+        <script src="http://code.jquery.com/ui/1.9.1/jquery-ui.js"></script>
 		<link rel="stylesheet" type="text/css" href="../source/loader.css">
 		<link rel="stylesheet" type="text/css" href="../source/dialog_blue.css">
 		<script src="../source/loader.js"></script>
-
+		<script src="../source/da.js"></script>
 	</head>
 	<body>
 		<div class="background">
