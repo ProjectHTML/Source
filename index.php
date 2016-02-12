7e664cbd3068216507a32531452444b3c91a8490
ProjectHTML <salehsonn@gmail.com>
2016-02-12 22:47:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*session_start();
+session_start();
 if(isset($_SESSION['user']))
 {
 	header("location: play.php");
@@ -8,8 +8,43 @@ else
 {
 	mysql_connect("localhost", "root", "");
 	mysql_select_db("ph");
-	
-} */
+	$error = '';
+	if (isset($_POST['register']))
+	{
+		header("location: register.php");
+	}
+	else if(isset($_POST['login']))
+	{
+		if (empty($_POST['username']))
+		{
+			$error = "<font color='red'>Username is invalid.</font>";
+		}
+		else if (empty($_POST['password']))
+		{
+			$error = "<font color='red'>Password is invalid.</font>";
+		}
+		else
+		{
+			$username=$_POST['username'];
+			$password=$_POST['password'];
+			$username = stripslashes($username);
+			$password = stripslashes($password);
+			$username = mysql_real_escape_string($username);
+			$password = mysql_real_escape_string($password);
+			$query = mysql_query("select * from users where password='$password' AND username='$username'");
+			$rows = mysql_num_rows($query);
+			if ($rows == 1)
+			{
+				$_SESSION['user']=$username;
+				header("location: play.php");
+			}
+			else
+			{
+				$error = "<font color='red'>Username or Password is invalid.</font>";
+			}
+		}
+	}
+}
 ?>
 <!doctype HTML>
 <html>
@@ -35,14 +70,15 @@ else
 			<br>
 			 &nbsp; &nbsp;<label>Username</label>
 			<br>
-			 &nbsp; &nbsp;<input id="input" name="username" type="text">
+			 &nbsp; &nbsp;<input id="input" name="username" type="text" style="width: 95%;">
 			<br>
 			 &nbsp; &nbsp;<label>Password</label>
 			<br>
-			 &nbsp; &nbsp;<input id="input" name="password" type="password">
+			 &nbsp; &nbsp;<input id="input" name="password" type="password" style="width: 95%;">
 			 <br>
+			 &nbsp;&nbsp;<?php echo $error; ?>
 			 <br>
-			 <center><input id="login" name="submit" type="submit" value="Login">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<input id="login" name="register" type="submit" value="Register"></center>
+			 <center><input id="login" name="login" type="submit" value="Login">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<input id="login" name="register" type="submit" value="Register"></center>
 			</form>
 		</div>
 	</body>
