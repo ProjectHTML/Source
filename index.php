db6ec0aeea8ae94835396bf6586f5401d59851a6
ProjectHTML <salehsonn@gmail.com>
2016-02-12 18:10:33 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype HTML>
+<html>
+	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="../source/loader.css">
+		<link rel="stylesheet" type="text/css" href="../source/dialog_blue.css">
+		<script src="../source/loader.js"></script>
+
+	</head>
+	<body>
+		<div class="background">
+			<div id="bar">
+				<div id="progress"></div>
+			</div>
+		</div>
+		<div class="login_box">
+			<form action="" method="post">
+			<br>
+			<br>
+			<br>
+			 &nbsp; &nbsp;<label>Username</label>
+			<br>
+			 &nbsp; &nbsp;<input id="input" name="username" type="text">
+			<br>
+			 &nbsp; &nbsp;<label>Password</label>
+			<br>
+			 &nbsp; &nbsp;<input id="input" name="password" type="password">
+			 <br>
+			 <br>
+			 <center><input id="login" name="submit" type="submit" value="Login">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<input id="login" name="register" type="submit" value="Register"></center>
+			</form>
+		</div>
+	</body>
+</html>
