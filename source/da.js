adb62c45a796d3f1dd1635eb11e9c28c76d8eeba
ProjectHTML <salehsonn@gmail.com>
2016-02-12 22:48:29 +0100

    Update da.js

--- source/da.js
+++ source/da.js
@@ -2,6 +2,18 @@ $(document).ready(function () {
    $('.login_box').draggable({
         opacity: 0.35,
         handle: '.login_box',
-		    containment: "screen"
+		containment: "screen"
+    });
+	
+	$('.register_box').draggable({
+        opacity: 0.35,
+        handle: '.register_box',
+		containment: "screen"
+    });
+	
+	$('.motd_box').draggable({
+        opacity: 0.35,
+        handle: '.motd_box',
+		containment: "screen"
     });
 });
